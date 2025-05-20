eacd5fe79c7b8f1272841cccd4d4a6b19946c56b
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2025-05-20 19:45:19 +0530

    recruiter model added

--- models/recruiter.models.js
+++ models/recruiter.models.js
@@ -1,11 +1,44 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 const recruiterSchema = new mongoose.Schema({
-
-
-
-
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profileImage:{
+        type:String,
+    },
+    
 });
+
+recruiterSchema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+recruiterSchema.methods.generateAuthToken = function(){
+    return jwt.sign({
+        fullName:this.fullName,
+        email:this.email,
+        _id:this._id
+    },process.env.AUTHORISATION_SECRET_KEY,{expiresIn:'24h'});
+}
+
+recruiterSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+};
+
+
 
 module.exports = mongoose.model('Recruiter',recruiterSchema);

c0fab13c0cb5c82b1d539a47e1adea6780dd5f7f
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2025-05-20 18:29:12 +0530

    middleware added in routes

--- routes/candidate.routes.js
+++ routes/candidate.routes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const {verifyJwt} = require('../middleware/auth.js');
 const {handleCandidateRegisterRoute,handleCandidateDeleteRoute,handleCandidateLoginRoute,handleCandidateLogoutRoute} = require('../controllers/candidate.controllers.js');
 
 
 // register routes
 router.post('/register',handleCandidateRegisterRoute);
 router.post('/login',handleCandidateLoginRoute);
-router.post('/logout',handleCandidateLogoutRoute);
-router.delete('/delete',handleCandidateDeleteRoute);
+router.post('/logout',verifyJwt,handleCandidateLogoutRoute);
+router.delete('/delete',verifyJwt,handleCandidateDeleteRoute);
 
 // uploads routes
-router.post('/upload/cover-image',handleCandidateCoverImageRoute);
-router.post('/uploads/resume',handleCandidateResumeUploadsRoute);
-router.post('/upload/update/cover-image',handleCandidateUpdateCoverImageRoute);
-router.post('/upload/update/resume',handleCandidateUpdateResumeRoute);
-router.delete('/upload/delete/resume',handleCandidateDeleteResumeRoute);
-router.delete('/upload/delete/cover-image',handleCandidateDeleteCoverImageRoute);
+router.post('/upload/cover-image',verifyJwt,handleCandidateCoverImageRoute);
+router.post('/uploads/resume',verifyJwt,handleCandidateResumeUploadsRoute);
+router.post('/upload/update/cover-image',verifyJwt,handleCandidateUpdateCoverImageRoute);
+router.post('/upload/update/resume',verifyJwt,handleCandidateUpdateResumeRoute);
+router.delete('/upload/delete/resume',verifyJwt,handleCandidateDeleteResumeRoute);
+router.delete('/upload/delete/cover-image',verifyJwt,handleCandidateDeleteCoverImageRoute);
 
 // job applying routes 
-router.get('/job-listing',handleCandidateJobListingRoute);
+router.get('/job-listing',verifyJwt,handleCandidateJobListingRoute);
 // job apply routes
 
 
